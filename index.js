827d896f09b70fe547c513877a41a2838fa34216
ken8waru1 <ken8waru1@gmail.com>
2019-12-22 00:14:44 -0800

    Complete bot base functionality

--- index.js
+++ index.js
@@ -3,23 +3,42 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 require('dotenv').config();
 
-const { fetchRandomArmorSet, fetchRandomWeapon, fetchRandomCharm, fetchRandomMonster } = require('./requests');
+const huntingRequests = require('./requests');
 
 client.on('message', async msg => {
   if (msg.content === '/roulette') {
-    //to be revisted and refactored to have concurrent requests
-    const randomArmorSet = await fetchRandomArmorSet();
-    const randomWeapon = await fetchRandomWeapon();
-    const randomMonster = await fetchRandomMonster();
-    const randomCharm = await fetchRandomCharm();
-    
-    msg.reply(`pray to the gacha \n \n 
-    **Head**: ${randomArmorSet.head.name} (rarity: ${randomArmorSet.head.rarity}) \n 
-    **Chest**: ${randomArmorSet.chest} (rarity: ${randomArmorSet.chest.rarity}) \n 
-    **Arms**: ${randomArmorSet.gloves} (rarity: ${randomArmorSet.gloves.rarity}) \n 
-    **Waist**: ${randomArmorSet.waist} (rarity: ${randomArmorSet.waist.rarity}) \n 
-    **Legs**: ${randomArmorSet.legs} (rarity: ${randomArmorSet.legs.rarity})  
-    `);
+    const promises = [];
+    const randomHunt = {
+      randomArmorSet: null,
+      randomWeapon: null,
+      randomCharm: null,
+      randomMonster: null,
+    };
+
+    for (let request of Object.values(huntingRequests)) {
+      promises.push(request());
+    }
+
+    await Promise.all(promises).then(randomFeatures => {
+      Object.keys(randomHunt).forEach((key, idx) => {
+        randomHunt[key] = randomFeatures[idx];
+      })
+    });
+
+    const { randomArmorSet, randomWeapon, randomCharm, randomMonster } = randomHunt;
+    let message = ''
+
+    //formatting with template literals causes weird things when the message hits discord, so doing it this way for now, but there has to be a better way so will revisit
+    message += `**Head**: ${randomArmorSet.head.name} (rarity: ${randomArmorSet.head.rarity}) \n`;
+    message += `**Chest**: ${randomArmorSet.chest.name} (rarity: ${randomArmorSet.chest.rarity}) \n`;
+    message += `**Arms**: ${randomArmorSet.gloves.name} (rarity: ${randomArmorSet.gloves.rarity}) \n`;
+    message += `**Waist**: ${randomArmorSet.waist.name} (rarity: ${randomArmorSet.waist.rarity}) \n`;
+    message += `**Legs**: ${randomArmorSet.legs.name} (rarity: ${randomArmorSet.legs.rarity}) \n`;
+    message += `**Weapon** :${randomWeapon.name} (rarity: ${randomWeapon.rarity}) \n`;
+    message += `**Charm**: ${randomCharm.name} (rarity: ${_.sample(randomCharm.ranks).rarity}) \n`;
+    message += `**Target**: ${randomMonster.name}`;
+
+    msg.reply(`pray to the gacha\n\n${message}`);
   }
 });
 
--- requests.js
+++ requests.js
@@ -5,7 +5,7 @@ const { ARMOR_TYPES } = require('./constants');
 const fetchRandomArmorSet = armorType => {
   return axios.get(`https://mhw-db.com/armor`).then(armorPieces => {
     const randomArmorSet = {};
-    
+
     for (armorType of ARMOR_TYPES) {
       const randomArmorPiece = _.sample(armorPieces.data.filter(armorPiece => armorPiece.type === armorType));
       _.merge(randomArmorSet, { [armorType]: randomArmorPiece })
@@ -22,13 +22,21 @@ const fetchRandomWeapon = () => {
   });
 }
 
+const fetchRandomCharm = () => {
+  return axios.get('https://mhw-db.com/charms').then(res => {
+    const randomCharm = _.sample(res.data);
+    return randomCharm;
+  });
+}
+
 const fetchRandomMonster = () => {
   return axios.get('https://mhw-db.com/monsters?q={"type": "large"}').then(res => {
-    const randomMonster = _sample(res.data);
+    const randomMonster = _.sample(res.data);
     return randomMonster;
   });
 }
 
 exports.fetchRandomArmorSet = fetchRandomArmorSet;
 exports.fetchRandomWeapon = fetchRandomWeapon;
+exports.fetchRandomCharm = fetchRandomCharm;
 exports.fetchRandomMonster = fetchRandomMonster;
